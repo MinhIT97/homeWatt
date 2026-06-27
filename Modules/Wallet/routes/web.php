a922f6a2b0d5d512e7b19c6e295d85d392cb246e
minh nguyen <minh06081997@gmail.com>
2026-06-27 21:46:19 +0700

    fiance

--- Modules/Wallet/routes/web.php
+++ Modules/Wallet/routes/web.php
@@ -10,7 +10,7 @@ Route::middleware(['web', 'auth', 'verified'])->group(function () {
     Route::get('wallets/{wallet}', [WalletController::class, 'show'])->name('wallets.show');
     Route::get('wallets/{wallet}/edit', [WalletController::class, 'edit'])->name('wallets.edit');
     Route::put('wallets/{wallet}', [WalletController::class, 'update'])->name('wallets.update');
-    Route::patch('wallets/{wallet}', [WalletController::class, 'update'])->name('wallets.update');
+    Route::patch('wallets/{wallet}', [WalletController::class, 'update']);
     Route::delete('wallets/{wallet}', [WalletController::class, 'destroy'])->name('wallets.destroy');
     Route::post('wallets/{wallet}/archive', [WalletController::class, 'archive'])->name('wallets.archive');
     Route::post('wallets/{wallet}/restore', [WalletController::class, 'restore'])->name('wallets.restore');
